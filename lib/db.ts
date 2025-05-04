2ffa65c10ba8b0a7c34087f1a3b947d22a19c2d4
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-05-04 09:48:00 +0530

    error will be fixed

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!; // in this '!' to make sure that the mogodb are present 
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if(!MONGODB_URI){
     throw new Error("Please add the MONGODB_URI in the .env")
@@ -9,35 +9,37 @@ if(!MONGODB_URI){
 // types defined in the types.d.ts
 let cached = global.mongoose;
 
-// if cached is not present than we will created | if our next is not run on any 'edge'
+// if cached is not present then we will create it
 if(!cached){
-    cached = global.mongoose = {conn:null, promise:null};
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
-// db connection function 
-
+// db connection function
 export async function connectionToDB(){
-    if(!cached.conn){
-        return cached.conn
+    // If we already have a connection, return it
+    if(cached.conn){
+        return cached.conn;
     }
-    // if we not able to find the promise that will we will create own promise
+    
+    // If a connection promise doesn't exist yet, create one
     if(!cached.promise){
         const opts = {
             bufferCommands: true,
             maxPoolSize: 10,
-        }
-    cached.promise = mongoose
-    .connect(MONGODB_URI, opts)
-    .then(()=> mongoose.connection);
+        };
+        
+        cached.promise = mongoose
+            .connect(MONGODB_URI, opts)
+            .then(() => mongoose.connection);
     }
-
+    
     try {
-        // wait for the connection
-        cached.conn = await cached.promise
+        // Wait for the connection
+        cached.conn = await cached.promise;
     } catch (error) {
         cached.promise = null;
-        throw error 
+        throw error;
     }
-
+    
     return cached.conn;
 }
